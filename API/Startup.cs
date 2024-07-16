13e470e71a290d039e13536233d4b17d072f6d8f
elyesbejaoui2001 <elyes.bejaoui@esprit.tn>
2024-07-16 15:11:46 +0100

    Initial Commit

---
+++ API/Startup.cs
@@ -0,0 +1,38 @@
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Startup
+{
+    private readonly IConfiguration _config;
+    public Startup(IConfiguration config)
+    {
+        _config = config;
+    }
+    public IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddDbContext<StoreContext>(x => 
+        x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
